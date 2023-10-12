758381d42a364cc3791ae0ec6ddaf01bbd3e90a3
Xenosort <147574211+Xenosort@users.noreply.github.com>
2023-10-12 14:31:50 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -766,34 +766,22 @@ function preloadImages() {
   const totalLength = characterDataToSort.length;
   let imagesLoaded = 0;
 
-  const loadImage = (src, idx) => {
-      return new Promise((resolve, reject) => {
-          const img = new Image();
-
-          img.crossOrigin = 'Anonymous';
-          img.onload = () => {
-            setImageToData(img, idx);
-            resolve(img);
-          };
-          img.onerror = img.onabort = () => reject(src);
-          if ( img.complete || img.complete === undefined ) {
-            img.src = src;
-          }
-          img.src = src;
-      });
+  const loadImage = async (src) => {
+    const blob = await fetch(src).then(res => res.blob());
+    return new Promise((res, rej) => {
+      const reader = new FileReader();
+      reader.onload = ev => {
+        progressBar(`Loading Image ${++imagesLoaded}`, Math.floor(imagesLoaded * 100 / totalLength));
+        res(ev.target.result);
+      };
+      reader.onerror = rej;
+      reader.readAsDataURL(blob);
+    });
   };
 
-  const setImageToData = (img, idx) => {
-    const canvas = document.createElement('canvas');
-    canvas.width = img.naturalWidth;
-    canvas.height = img.naturalHeight;
-    canvas.getContext('2d').drawImage(img, 0, 0);
-    characterDataToSort[idx].img = canvas.toDataURL();
-    progressBar(`Loading Image ${++imagesLoaded}`, Math.floor(imagesLoaded * 100 / totalLength));
-  };
-
-  const promises = characterDataToSort.map((char, idx) => loadImage(imageRoot + char.img, idx));
-  return Promise.all(promises);
+  return Promise.all(characterDataToSort.map(async (char, idx) => {
+    characterDataToSort[idx].img = await loadImage(imageRoot + char.img);
+  }));
 }
 
 /**
